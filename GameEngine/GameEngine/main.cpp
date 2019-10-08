6f892d1907928e2c9f4184b95b746ae95d73afd4
WillPritchatt <43639356+WillPritchatt@users.noreply.github.com>
2019-10-08 12:09:27 +0100

    Input Functions

--- GameEngine/GameEngine/main.cpp
+++ GameEngine/GameEngine/main.cpp
@@ -5,21 +5,36 @@ void main()
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window* p_window = SDL_CreateWindow("My win", 0, 0, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* p_window = SDL_CreateWindow("My win", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+
 
 	SDL_Renderer* p_Renderer = SDL_CreateRenderer(p_window, -1, SDL_RENDERER_ACCELERATED);
 
 	SDL_SetRenderDrawColor(p_Renderer, 0, 0, 128, 0);
 
-	SDL_ShowWindow(p_window);
+	//SDL_ShowWindow(p_window);
 
-	while (true)
+	bool quit = false;
+
+	while (!quit)
 	{
+		SDL_Event E;
+		while (SDL_PollEvent(&E)) 
+		{
+			if (E.type == SDL_KEYDOWN) 
+			{
+				if (E.key.keysym.sym == SDLK_d) 
+				{
+					quit = true;
+				}
+			}
+		}
+
 		SDL_RenderClear(p_Renderer);
 
 		SDL_SetRenderDrawColor(p_Renderer, 0, 0, 128, 0);
 
-		//SDL_RenderPresent(p_Renderer);
+		SDL_RenderPresent(p_Renderer);
 
 		SDL_Delay(16);
 
